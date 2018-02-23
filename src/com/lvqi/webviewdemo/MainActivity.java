a7bdaa811d346420900cdab146a105763bdcd9e0
lvqi <hblvqi@163.com>
2018-02-23 22:28:22 +0800

    show baidu on webview

--- src/com/lvqi/webviewdemo/MainActivity.java
+++ src/com/lvqi/webviewdemo/MainActivity.java
@@ -1,34 +1,28 @@
 package com.lvqi.webviewdemo;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity {
 
+	private WebView mWebView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mWebView=(WebView) findViewById(R.id.webview);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.setWebViewClient(new WebViewClient());
+		mWebView.loadUrl("https://www.baidu.com");
+		
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
